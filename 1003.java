3b40cd3f3c7aca3da5c6d3669520e2015b59579a
Weverton V. Castro <wvcastro42@gmail.com>
2013-10-16 14:58:30 -0300

    Create 1003.java

---
+++ 1003.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+ 
+ 
+public class Main {
+    public static void main(String[] args) throws java.lang.Exception {
+         
+        Scanner scan = new Scanner(System.in);
+        int A, B, SOMA;
+        A = scan.nextInt();
+        B = scan.nextInt();
+        SOMA = A + B;
+        System.out.println("SOMA = "+SOMA);
+    }
+}
