60c62aff388760c7dd056d123c56b95a674855ce
Weverton V. Castro <wvcastro42@gmail.com>
2013-10-16 14:55:32 -0300

    Create 1001.java
    1001 - Extremamente Básico

---
+++ 1001.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main{
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        int a, b, x;
+        a = scan.nextInt();
+        b = scan.nextInt();
+        x = a+b;
+ 
+        System.out.println("X = "+x);
+    }
+}
